dababdda50e1915acbb0a982a4a3dc40b854aabd
Ashutosh Gautam <aashutoshgautam2611@gmail.com>
2021-09-14 10:10:03 +0530

    Added the experience section

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
-import IndexPage from "./components/IndexPage";
 import Profile from "./components/Profile";
 import Footer from "./components/Resume/Footer";
 import Header from "./components/Resume/Header";
-import MainLayout from "./components/Resume/MainLayout";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <Router>
-      <IndexPage/>
+      <Header/>
       <Profile/>
+      <Experience/>
       <Skills/>
       <Education/>
       <Projects/>
